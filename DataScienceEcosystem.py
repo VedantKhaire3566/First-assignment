5fa06c149bb09da273f952f05397b2b30aedd092
VedantKhaire3566 <vedantkhaire3566@gmail.com>
2024-06-15 13:15:54 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Data Science Basics Notebook
+
+
+# In[2]:
+
+
+## Introduction
+This notebook serves as an introduction to various basic concepts and tools in data science. It will cover programming languages, libraries, tools, and some simple arithmetic operations commonly used in the field.
+
+
+# In[3]:
+
+
+## Data Science Languages
+- Python
+- R
+- SQL
+- Julia
+- Scala
+
+
+# In[4]:
+
+
+## Data Science Libraries
+- NumPy
+- Pandas
+- Matplotlib
+- Scikit-learn
+- TensorFlow
+- Keras
+- PyTorch
+
+
+# In[7]:
+
+
+## Data Science Tools
+| Tool            | Description                                      |
+|-----------------|--------------------------------------------------|
+| Jupyter Notebook| Interactive computing environment                |
+| RStudio         | Integrated development environment for R         |
+| Apache Spark    | Cluster-computing framework                      |
+| TensorFlow      | Open-source machine learning library             |
+| Tableau         | Data visualization tool                          |
+
+
+# In[8]:
+
+
+## Arithmetic Expression Examples
+Arithmetic expressions are used to perform various mathematical operations. Here are some basic examples.
+
+
+# In[9]:
+
+
+# Multiplication and addition
+a = 5
+b = 3
+result = (a * b) + b
+result
+
+
+# In[10]:
+
+
+# Conversion from minutes to hours
+minutes = 150
+hours = minutes / 60
+hours
+
+
+# In[12]:
+
+
+## Objectives
+- Understand the basics of data science languages and libraries
+- Learn about different data science tools
+- Perform basic arithmetic operations in Python
+
+
+# In[13]:
+
+
+## Author
+Vedant Khaire
+
+
+# In[ ]:
+
+
+
+
